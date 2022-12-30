340db21cb42cd1639df260ee36b6fbd5011ed9d9
MD AL AMIN <mdalaminbey@gmail.com>
2022-12-30 17:12:40 +0600

    add app:setup command

--- src/Artisan.php
+++ src/Artisan.php
@@ -3,60 +3,25 @@
 namespace Wpcommander\Artisan;
 
 use Symfony\Component\Console\Application;
+use Wpcommander\Artisan\Commands\Setup;
 
 class Artisan
 {
     public static function exec( $rootDir )
     {
-
         $application = new Application();
 
-        $application->add( new Setup );
+        foreach ( self::commands() as $command ) {
+            $application->add( new $command( $rootDir ) );
+        }
 
         $application->run();
-
-        // $pluginNewName = static::run( "Enter plugin name: " );
-        // $pluginNewName = PluginName::pluginNameValidation( $pluginNewName );
-
-        // $pluginNewNameSpace = static::run( "Enter plugin namespace: " );
-        // $pluginNewNameSpace = PluginNamespace::pluginNameSpaceValidation( $pluginNewNameSpace );
-
-        // $folders = ['app', 'bootstrap', 'config', 'enqueues', 'routes'];
-        // $data    = [
-        //     'pluginName'         => 'PluginName',
-        //     'pluginNewName'      => $pluginNewName,
-        //     'pluginNamespace'    => 'PluginNameSpace',
-        //     'pluginNewNamespace' => $pluginNewNameSpace,
-        //     'apiNamespace'       => 'plugin-api-namespace',
-        //     'apiNewNamespace'    => 'myplugin'
-        // ];
-
-        // foreach ( $folders as $folder ) {
-        //     $dir = $rootDir . '\\' . $folder;
-        //     Common::getDirContents( $dir, $data );
-        // }
-
-        // $dir     = $rootDir . '\composer.json';
-        // $content = Common::getReplaceContent( $dir, $data );
-        // $content = str_replace( '"post-create-project-cmd" : "php artisan"', '', $content );
-        // file_put_contents( $dir, $content );
-
-        // $content      = Common::getReplaceContent( $rootDir . '\wpcommander.php', $data );
-        // $rootFileName = str_replace( ' ', '-', strtolower( $pluginNewName ) ) . '.php';
-        // $file         = fopen( $rootFileName, "wb" );
-
-        // fwrite( $file, $content );
-        // fclose( $file );
-
-        // exec( 'composer remove --dev wpcommander/artisan' );
-        // exec( 'composer dump-autoload' );
-
-        // unlink( $rootDir . '\wpcommander.php' );
-        // unlink( $rootDir . '\artisan' );
     }
 
-    public static function run( $text )
+    public static function commands()
     {
-        return readline( $text );
+        return [
+            Setup::class
+        ];
     }
 }
---
+++ src/Commands/Setup.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Wpcommander\Artisan\Commands;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+// the name of the command is what users type after "php bin/console"
+#[AsCommand(
+    name:'app:setup',
+    description:'Setup wordpress plugin basic information',
+)]
+class Setup extends Command
+{
+    protected $rootDir;
+
+    public function __construct( string $rootDir )
+    {
+        $this->rootDir = $rootDir;
+        parent::__construct();
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output ): int
+    {
+        $helper = $this->getHelper( 'question' );
+
+        /**
+         * Get Plugin Name
+         */
+        $question   = new Question( 'Enter plugin name: ' );
+        $pluginName = $helper->ask( $input, $output, $question );
+        $pluginName = $this->pluginNameValidation( $pluginName, $input, $output );
+
+        /**
+         * Get Plugin Namespace
+         */
+        $question        = new Question( 'Enter plugin namespace: ' );
+        $pluginNameSpace = $helper->ask( $input, $output, $question );
+        $pluginNameSpace = $this->pluginNameSpaceValidation( $pluginNameSpace, $input, $output );
+
+        /**
+         * Get Plugin Rest Api Namespace
+         */
+        $question           = new Question( 'Enter plugin api namespace: ' );
+        $pluginApiNameSpace = $helper->ask( $input, $output, $question );
+        $pluginApiNameSpace = $this->pluginApiNamespaceValidation( $pluginApiNameSpace, $input, $output );
+
+        $data = [
+            'pluginName'         => 'PluginName',
+            'pluginNewName'      => $pluginName,
+            'pluginNamespace'    => 'PluginNameSpace',
+            'pluginNewNamespace' => $pluginNameSpace,
+            'apiNamespace'       => 'plugin-api-namespace',
+            'apiNewNamespace'    => $pluginApiNameSpace
+        ];
+
+        foreach ( $this->folders() as $folder ) {
+            $dir = $this->rootDir . '\\' . $folder;
+            Common::getDirContents( $dir, $data );
+        }
+
+        return Command::SUCCESS;
+    }
+
+    public function updateInformation( array $data )
+    {
+        $dir = $this->rootDir . '\composer.json';
+        Common::updatePluginInfo( $dir, $data );
+
+        $content      = Common::getReplaceContent( $this->rootDir . '\wpcommander.php', $data );
+        $rootFileName = str_replace( ' ', '-', strtolower( $data['pluginNewName'] ) ) . '.php';
+        $file         = fopen( $rootFileName, "wb" );
+
+        fwrite( $file, $content );
+        fclose( $file );
+
+        exec( 'composer dump-autoload' );
+
+        unlink( $this->rootDir . '\wpcommander.php' );
+    }
+
+    protected function configure(): void
+    {
+        $this->setHelp( 'Setup wordpress plugin basic information' );
+    }
+
+    protected function folders()
+    {
+        return ['app', 'bootstrap', 'config', 'enqueues', 'routes'];
+    }
+
+    protected function pluginNameValidation( $pluginName, $input, $output )
+    {
+        if ( empty( $pluginName ) ) {
+            $question   = new Question( "Please enter plugin name: " );
+            $helper     = $this->getHelper( 'question' );
+            $pluginName = $helper->ask( $input, $output, $question );
+            $pluginName = $this->pluginNameValidation( $pluginName, $input, $output );
+        }
+
+        return $pluginName;
+    }
+
+    protected function pluginApiNamespaceValidation( $pluginApiNamespace, $input, $output )
+    {
+        if ( empty( $pluginApiNamespace ) ) {
+            $question           = new Question( "Please enter plugin api namespace: " );
+            $helper             = $this->getHelper( 'question' );
+            $pluginApiNamespace = $helper->ask( $input, $output, $question );
+            $pluginApiNamespace = $this->pluginApiNamespaceValidation( $pluginApiNamespace, $input, $output );
+        }
+
+        return $pluginApiNamespace;
+    }
+
+    public function pluginNameSpaceValidation( $pluginNameSpace, $input, $output )
+    {
+        if ( empty( $pluginNameSpace ) ) {
+            $question        = new Question( "Please enter plugin namespace: " );
+            $helper          = $this->getHelper( 'question' );
+            $pluginNameSpace = $helper->ask( $input, $output, $question );
+            $pluginNameSpace = $this->pluginNameSpaceValidation( $pluginNameSpace, $input, $output );
+        }
+
+        $first_character = intval( substr( $pluginNameSpace, 0, 1 ) );
+
+        if ( 0 !== $first_character ) {
+            $question        = new Question( "Please enter a valid namespace: " );
+            $helper          = $this->getHelper( 'question' );
+            $pluginNameSpace = $helper->ask( $input, $output, $question );
+            $pluginNameSpace = $this->pluginNameSpaceValidation( $pluginNameSpace, $input, $output );
+        }
+
+        return $pluginNameSpace;
+    }
+}
