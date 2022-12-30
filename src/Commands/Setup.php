9f36ae63a18694d79d82340ca7a846b45f9c9809
MD AL AMIN <mdalaminbey@gmail.com>
2022-12-30 17:23:56 +0600

    Update Setup.php

--- src/Commands/Setup.php
+++ src/Commands/Setup.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Wpcommander\Artisan\Common;
 
 // the name of the command is what users type after "php bin/console"
 #[AsCommand(
